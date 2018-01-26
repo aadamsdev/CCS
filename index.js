f92ebc865260a1fed61b02d0cc3b7163dd9c9aed
Andrew Adams <aadamsdev@gmail.com>
2018-01-25 21:56:18 -0500

    modifying file structure

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 'use strict';
 
-const Geofence = require('./geofence')
-const GeofenceManager = require('./geofenceManager')
-const SocketEvents = require('./socketEvents')
 const MongoClient = require('mongodb').MongoClient;
-const MongoConfig = require('./mongoConfig')
-const ChatHistory = require('./chatHistory')
+const MongoConfig = require('./config/mongoConfig')
 
+const ChatSocket = require('./websocket/chatSocket')
 const ObjectId = require('mongodb').ObjectID;
 
 const bodyParser = require('body-parser')
@@ -16,8 +13,7 @@ const http = require('http').createServer(app)
 const io = require('socket.io')(http)
 const port = process.env.PORT || 8000;
 
-const geofenceManager = new GeofenceManager('./boundaries/neighbourhoods.json')
-let db = null
+
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -31,64 +27,9 @@ MongoClient.connect(MongoConfig.db.uri, function (err, client) {
         console.log(err)
     } else {
         console.log("Connected successfully to mongodb server");
-        db = client.db(MongoConfig.db.name)
+        const db = client.db(MongoConfig.db.name)
+
+        const chatSocket = new ChatSocket()
+        chatSocket.registerSocketEvents(io, db)
     }
 })
-
-io.on(SocketEvents.connection, function (socket) {
-    console.log('a user connected ' + socket.id)
-
-    // Client location update
-    socket.on(SocketEvents.location_update, function (locationUpdate) {
-        console.log(locationUpdate)
-
-        // Check if client is in previously known geofence; for performance purposes
-        if (locationUpdate.lastKnownChatRoom
-            && geofenceManager.contains(locationUpdate.lastKnownChatRoom)
-            && geofenceManager.getGeofence(locationUpdate.lastKnownChatRoom).containsPoint(locationUpdate)) {
-
-            const chatRoom = locationUpdate.lastKnownChatRoom
-            socket.join(chatRoom)
-            sendChatroomUpdate(io, socket, db, chatRoom)
-        } else { // Otherwise loop find the geofence containing user's location
-            const geofence = geofenceManager.getGeofenceContainingPoint(locationUpdate)
-            if (geofence) {
-                const chatRoom = geofence.name
-                socket.join(chatRoom)
-                sendChatroomUpdate(io, socket, db, chatRoom)
-            } else {
-                // TODO: ADD HANDLING FOR USERS OUTSIDE TORONTO
-            }
-        }
-    })
-
-    // Send received message
-    socket.on(SocketEvents.outgoing_message, function (message) {
-        console.log(message)
-        const chatRoom = message.chatRoomName
-        message['timestamp'] = new Date()
-
-        ChatHistory.putForChatRoom(db, message, (updatedMessage) => {
-            socket.join(chatRoom)
-            io.to(chatRoom).emit(SocketEvents.incoming_message, updatedMessage);
-        })
-    })
-
-    // Disconnection
-    socket.on(SocketEvents.disconnect, function () {
-        console.log('user disconnected')
-    })
-})
-
-function sendChatroomUpdate(io, socket, db, chatRoom) {
-    ChatHistory.getForChatRoomUpdate(db, chatRoom, (chatHistory) => {
-        console.log(chatHistory)
-
-        io.to(socket.id).emit(SocketEvents.chatroom_update, {
-            chatRoomName: chatRoom,
-            messages: chatHistory
-        })
-    }, (err) => {
-        console.log(err)
-    })
-}
